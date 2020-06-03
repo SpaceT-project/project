bba23105655498b74b87b56ea1dfd6d06110bdb0
Huyen059 <samihuyen059@gmail.com>
2020-06-03 15:49:17 +0200

    adding basic html and css for contact page

---
+++ dist/js/navbar.js
@@ -0,0 +1,21 @@
+//Function for the menu button of navigation bar
+const navBurger = document.querySelector('.navBurger');
+const navBar = document.querySelector('.navBar');
+
+navBurger.addEventListener('click', toggleNavMenu);
+
+let showMenu = false;
+
+function toggleNavMenu(){
+    if(!showMenu){
+        navBurger.innerHTML = '<i class="fas fa-times"></i>';
+        navBurger.style.color = 'white';
+        navBar.classList.add('open');
+        showMenu = true;
+    } else {
+        navBurger.innerHTML = '<i class="navBurgerBtn fas fa-bars"></i>';
+        navBurger.style.color = 'black';
+        navBar.classList.remove('open');
+        showMenu = false;
+    }
+}
